e85df86e1c452aa490fa302cb6503e9253bfc772
Cameron Bytheway <cameron@nujii.com>
2013-05-06 22:27:11 -0600

    Use chaining style

--- app.js
+++ app.js
@@ -12,10 +12,23 @@ var app = module.exports = express();
 /**
  * Features
  */
-// Split test/AB test (defaults to [false, true])
-experiments.feature("validate");
+// Split/AB test (defaults to [false, true])
+experiments
+  .feature("validate")
+  .wip() // Mark the code as unstable or incomplete
+  .create();
+
 // Multi-variant
-experiments.feature("loginButtonColor", ["red", "blue", "green", "yellow"]);
+experiments
+  .feature("loginButtonColor")
+  .variants(["red", "blue", "green", "yellow"])
+  .create();
+
+// Named AB
+experiments
+  .feature("welcomeMessage")
+  .variants(["message1", "message2"])
+  .create();
 
 /**
  * Configure our app
@@ -36,17 +49,16 @@ app.configure(function(){
 });
 
 app.get("/", function(req, res, next){
-  // We can do ad-hoc features
-  res.feature("welcomeMessage", ["message1", "message2"], function(err) {
-    if(err) return next(err);
-    
-    if(res.locals.features.validate) {
-      // Our validate feature is turned on
-    }
-    else {
-      // It's turned off
-    }
-    
-    res.render("view");
-  });
+  if(res.feature("validate")) {
+    // Our validate feature is turned on
+  }
+  else {
+    // It's turned off
+  }
+
+  res.render("view");
+});
+
+app.get("/test", function(req, res) {
+  res.render("test");
 });
